8a7879e3d456718d4346dbec3a5273716f2881bc
ilies ouldmenouer <63064471+ilies-space@users.noreply.github.com>
2021-04-13 23:35:46 +0100

    display memberslist

--- app/screens/ManageMembers.js
+++ app/screens/ManageMembers.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, Text, View} from 'react-native';
 
 export default function ManageMembers() {
+  const [members, setmembers] = useState([
+    {
+      fullName: 'ilies ouldm',
+      carLicenceNumber: '123456',
+      carModel: 'bmw',
+    },
+  ]);
+
+  const renderMembers = ({item}) => {
+    return (
+      <View
+        style={{
+          borderWidth: 0.5,
+          margin: 10,
+        }}>
+        <Text>{item.fullName}</Text>
+        <Text>{item.carLicenceNumber}</Text>
+        <Text>{item.carModel}</Text>
+      </View>
+    );
+  };
   return (
     <View>
-      <Text>ManageMembers</Text>
+      <FlatList
+        renderItem={renderMembers}
+        keyExtractor={() => Math.random().toString()}
+        data={members}
+      />
     </View>
   );
 }
