ff844a4fe8af95a45d57256e6c407f1b33fb1a91
ilies ouldmenouer <63064471+ilies-space@users.noreply.github.com>
2021-04-13 23:30:30 +0100

    navigate beetwin screens

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
 
-export default function Home() {
+export default function Home({navigation}) {
   return (
     <View>
       <Text>HOME</Text>
 
-      <Button title={'scan car'} />
+      <Button
+        title={'scan car'}
+        onPress={() => {
+          navigation.push('ScanLicense');
+        }}
+      />
 
-      <Button title={'subscribed members list'} />
+      <Button
+        title={'subscribed members list'}
+        onPress={() => {
+          navigation.push('ManageMembers');
+        }}
+      />
     </View>
   );
 }
