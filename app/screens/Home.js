d885251c2d72e7399dd8553c8e15ddd2e0a6b310
ilies ouldmenouer <63064471+ilies-space@users.noreply.github.com>
2021-04-13 23:15:01 +0100

    initial commit

---
+++ app/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>HOME</Text>
+
+      <Button title={'scan car'} />
+
+      <Button title={'subscribed members list'} />
+    </View>
+  );
+}
